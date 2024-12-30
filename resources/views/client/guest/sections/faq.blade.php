7862d1fc940ed513ca19f60848c44a12bcd139a4
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2024-12-30 18:25:09 +0700

    Fix : FAQ Section

--- resources/views/client/guest/sections/faq.blade.php
+++ resources/views/client/guest/sections/faq.blade.php
@@ -10,7 +10,7 @@
                     class="flex items-center justify-between w-full py-5 font-medium rtl:text-right text-gray-500 border-b border-gray-200 gap-3"
                     data-accordion-target="#accordion-flush-body-1" aria-expanded="false"
                     aria-controls="accordion-flush-body-1">
-                    <span>What is Flowbite?</span>
+                    <span>Apakah tools yang digunakan untuk desain UI/UX bebas?</span>
                     <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -20,13 +20,8 @@
             </h2>
             <div id="accordion-flush-body-1" class="hidden" aria-labelledby="accordion-flush-heading-1">
                 <div class="py-5 border-b border-gray-200 text-start">
-                    <p class="mb-2 text-gray-500">Flowbite is an open-source library of interactive
-                        components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.
+                    <p class="mb-2 text-gray-500">Ya, peserta bebas menggunakan tools desain seperti Figma, Adobe XD, Sketch, atau software lain yang relevan.
                     </p>
-                    <p class="text-gray-500">Check out this guide to learn how to <a
-                            href="/docs/getting-started/introduction/"
-                            class="text-blue-600">get started</a> and start
-                        developing websites even faster with components on top of Tailwind CSS.</p>
                 </div>
             </div>
             <h2 id="accordion-flush-heading-2">
@@ -34,7 +29,7 @@
                     class="flex items-center justify-between w-full py-5 font-medium rtl:text-right text-gray-500 border-b border-gray-200 gap-3"
                     data-accordion-target="#accordion-flush-body-2" aria-expanded="false"
                     aria-controls="accordion-flush-body-2">
-                    <span>Is there a Figma file available?</span>
+                    <span>Apakah tim peserta diperbolehkan beranggotakan lebih dari dua orang?</span>
                     <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -44,12 +39,7 @@
             </h2>
             <div id="accordion-flush-body-2" class="hidden" aria-labelledby="accordion-flush-heading-2">
                 <div class="py-5 border-b border-gray-200 text-start">
-                    <p class="mb-2 text-gray-500">Flowbite is first conceptualized and designed using
-                        the Figma software so everything you see in the library has a design equivalent in our Figma
-                        file.</p>
-                    <p class="text-gray-500">Check out the <a href="https://flowbite.com/figma/"
-                            class="text-blue-600 hover:underline">Figma design system</a> based on
-                        the utility classes from Tailwind CSS and components from Flowbite.</p>
+                    <p class="mb-2 text-gray-500">Tidak, maksimal jumlah anggota tim untuk lomba UI/UX adalah tiga orang.</p>
                 </div>
             </div>
             <h2 id="accordion-flush-heading-3">
@@ -57,7 +47,7 @@
                     class="flex items-center justify-between w-full py-5 font-medium rtl:text-right text-gray-500 border-b border-gray-200 gap-3"
                     data-accordion-target="#accordion-flush-body-3" aria-expanded="false"
                     aria-controls="accordion-flush-body-3">
-                    <span class="text-start">What are the differences between Flowbite and Tailwind UI?</span>
+                    <span class="text-start">Apakah prototype harus interaktif atau cukup berupa mockup?</span>
                     <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -67,20 +57,61 @@
             </h2>
             <div id="accordion-flush-body-3" class="hidden" aria-labelledby="accordion-flush-heading-3">
                 <div class="py-5 border-b border-gray-200 text-start">
-                    <p class="mb-2 text-gray-500">The main difference is that the core components
-                        from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product.
-                        Another difference is that Flowbite relies on smaller and standalone components, whereas
-                        Tailwind UI offers sections of pages.</p>
-                    <p class="mb-2 text-gray-500">However, we actually recommend using both Flowbite,
-                        Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you from using the
-                        best of two worlds.</p>
-                    <p class="mb-2 text-gray-500">Learn more about these technologies:</p>
-                    <ul class="ps-5 text-gray-500 list-disc">
-                        <li><a href="https://flowbite.com/pro/"
-                                class="text-blue-600 hover:underline">Flowbite Pro</a></li>
-                        <li><a href="https://tailwindui.com/" rel="nofollow"
-                                class="text-blue-600 hover:underline">Tailwind UI</a></li>
-                    </ul>
+                    <p class="mb-2 text-gray-500">Penilaian akan lebih tinggi jika prototype interaktif, tetapi peserta dapat mengirimkan mockup jika tidak memungkinkan untuk membuat prototype interaktif.</p>
+                </div>
+            </div>
+            <h2 id="accordion-flush-heading-4">
+                <button type="button"
+                    class="flex items-center justify-between w-full py-5 font-medium rtl:text-right text-gray-500 border-b border-gray-200 gap-3"
+                    data-accordion-target="#accordion-flush-body-4" aria-expanded="false"
+                    aria-controls="accordion-flush-body-4">
+                    <span class="text-start">Apakah peserta boleh menggunakan framework dalam pengembangan website?</span>
+                    <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M9 5 5 1 1 5" />
+                    </svg>
+                </button>
+            </h2>
+            <div id="accordion-flush-body-4" class="hidden" aria-labelledby="accordion-flush-heading-4">
+                <div class="py-5 border-b border-gray-200 text-start">
+                    <p class="mb-2 text-gray-500">Tentu, peserta bebas menggunakan framework seperti Laravel, React, Next.js, atau lainnya sesuai dengan kebutuhan proyek.</p>
+                </div>
+            </div>
+            <h2 id="accordion-flush-heading-5">
+                <button type="button"
+                    class="flex items-center justify-between w-full py-5 font-medium rtl:text-right text-gray-500 border-b border-gray-200 gap-3"
+                    data-accordion-target="#accordion-flush-body-5" aria-expanded="false"
+                    aria-controls="accordion-flush-body-5">
+                    <span class="text-start">Apakah ada tema tertentu untuk website yang harus dibuat?</span>
+                    <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M9 5 5 1 1 5" />
+                    </svg>
+                </button>
+            </h2>
+            <div id="accordion-flush-body-5" class="hidden" aria-labelledby="accordion-flush-heading-5">
+                <div class="py-5 border-b border-gray-200 text-start">
+                    <p class="mb-2 text-gray-500">Ya, tema lomba akan diumumkan bersamaan dengan technical meeting, dan peserta harus mengikuti tema tersebut dalam pengembangan website.</p>
+                </div>
+            </div>
+            <h2 id="accordion-flush-heading-6">
+                <button type="button"
+                    class="flex items-center justify-between w-full py-5 font-medium rtl:text-right text-gray-500 border-b border-gray-200 gap-3"
+                    data-accordion-target="#accordion-flush-body-6" aria-expanded="false"
+                    aria-controls="accordion-flush-body-6">
+                    <span class="text-start">Apakah hosting website wajib dilakukan?</span>
+                    <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M9 5 5 1 1 5" />
+                    </svg>
+                </button>
+            </h2>
+            <div id="accordion-flush-body-6" class="hidden" aria-labelledby="accordion-flush-heading-6">
+                <div class="py-5 border-b border-gray-200 text-start">
+                    <p class="mb-2 text-gray-500">Tidak wajib, tetapi peserta yang meng-hosting websitenya akan mendapatkan poin tambahan pada penilaian teknis.</p>
                 </div>
             </div>
         </div>
