59953ba6cf364458c4c0cdfe5d6be1ffab1b7898
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-01-01 14:44:10 +0700

    Fix : Responsive section timeline

--- resources/views/client/guest/sections/timeline.blade.php
+++ resources/views/client/guest/sections/timeline.blade.php
@@ -4,8 +4,8 @@
         <h1 class="md:mb-4 text-3xl font-semibold tracking-wide text-white lg:px-12 md:text-6xl 2xl:text-7xl">Timeline
         </h1>
         <div class="mt-10">
-            <ol class="items-center sm:flex">
-                <li class="relative mb-6 sm:mb-0">
+            <ol class="items-center lg:flex">
+                <li class="relative mb-6 lg:mb-0">
                     <div class="flex items-center">
                         <div
                             class="z-10 flex items-center justify-center w-10 h-10 bg-blue-100 rounded-full ring-0 ring-white shrink-0">
@@ -15,16 +15,16 @@
                                     d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
                             </svg>
                         </div>
-                        <div class="hidden sm:flex w-full bg-gray-200 h-0.5"></div>
+                        <div class="hidden lg:flex w-full bg-gray-200 h-0.5"></div>
                     </div>
-                    <div class="mt-3 sm:pe-8 text-start">
+                    <div class="mt-3 lg:pe-8 text-start">
                         <h3 class="text-lg font-semibold text-white mb-2">Pendaftaran</h3>
                         <time
                             class="block mb-2 text-sm font-normal leading-none text-gray-400">6 Januari - 5 Februari 2025</time>
                         <p class="text-base font-normal text-gray-500">Peserta melakukan pendaftaran dan melengkapi berkas</p>
                     </div>
                 </li>
-                <li class="relative mb-6 sm:mb-0">
+                <li class="relative mb-6 lg:mb-0">
                     <div class="flex items-center">
                         <div
                             class="z-10 flex items-center justify-center w-10 h-10 bg-blue-100 rounded-full ring-0 ring-white">
@@ -34,16 +34,16 @@
                                     d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
                             </svg>
                         </div>
-                        <div class="hidden sm:flex w-full bg-gray-200 h-0.5"></div>
+                        <div class="hidden lg:flex w-full bg-gray-200 h-0.5"></div>
                     </div>
-                    <div class="mt-3 sm:pe-8 text-start">
+                    <div class="mt-3 lg:pe-8 text-start">
                         <h3 class="text-lg font-semibold text-white mb-2">Pengumpulan Karya</h3>
                         <time
                             class="block mb-2 text-sm font-normal leading-none text-gray-400">5 Februari - 6 Maret 2025</time>
                         <p class="text-base font-normal text-gray-500">Peserta mengunggah karya pada akun masing - masing</p>
                     </div>
                 </li>
-                <li class="relative mb-6 sm:mb-0">
+                <li class="relative mb-6 lg:mb-0">
                     <div class="flex items-center">
                         <div
                             class="z-10 flex items-center justify-center w-10 h-10 bg-blue-100 rounded-full ring-0 ring-white">
@@ -53,16 +53,16 @@
                                     d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
                             </svg>
                         </div>
-                        <div class="hidden sm:flex w-full bg-gray-200 h-0.5"></div>
+                        <div class="hidden lg:flex w-full bg-gray-200 h-0.5"></div>
                     </div>
-                    <div class="mt-3 sm:pe-8 text-start">
+                    <div class="mt-3 lg:pe-8 text-start">
                         <h3 class="text-lg font-semibold text-white mb-2">Penjurian</h3>
                         <time
                             class="block mb-2 text-sm font-normal leading-none text-gray-400">6 - 7 Maret 2025</time>
                         <p class="text-base font-normal text-gray-500">Karya yang sudah diunggah akan dinilai oleh juri</p>
                     </div>
                 </li>
-                <li class="relative mb-6 sm:mb-0">
+                <li class="relative mb-6 lg:mb-0">
                     <div class="flex items-center">
                         <div
                             class="z-10 flex items-center justify-center w-10 h-10 bg-blue-100 rounded-full ring-0 ring-white shrink-0">
@@ -72,9 +72,9 @@
                                     d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
                             </svg>
                         </div>
-                        <div class="hidden sm:flex w-full bg-accent h-0.5"></div>
+                        <div class="hidden lg:flex w-full bg-accent h-0.5"></div>
                     </div>
-                    <div class="mt-3 sm:pe-8 text-start">
+                    <div class="mt-3 lg:pe-8 text-start">
                         <h3 class="text-lg font-semibold text-white mb-2">Pengumuman</h3>
                         <time
                             class="block mb-2 text-sm font-normal leading-none text-gray-400">8 Maret  2025</time>
