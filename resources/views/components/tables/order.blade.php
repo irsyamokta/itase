5490c7c8045d5ea1a72de89a927cefe629b413b8
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-01-06 23:40:46 +0700

    Add : Component tables

---
+++ resources/views/components/tables/order.blade.php
@@ -0,0 +1,72 @@
+<div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-5 border-2 border-accent">
+    <x-searching :title="'Data Pembayaran Lomba'" :route="'dashboard'"/>
+    <div class="px-5">
+        <table class="w-full text-sm text-center rtl:text-right text-gray-500">
+            <thead class="text-xs text-white uppercase bg-primary">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Id Transaksi
+                    </th>
+                    <th scope="col" class="px-5 py-3">
+                        Nama Lengkap
+                    </th>
+                    <th scope="col" class="px-5 py-3 whitespace-nowrap">
+                        Email
+                    </th>
+                    <th scope="col" class="px-3 py-3">
+                        Phone
+                    </th>
+                    <th scope="col" class="px-5 py-3 whitespace-nowrap">
+                        Kategori Lomba
+                    </th>
+                    <th scope="col" class="px-5 py-3">
+                        Total Bayar
+                    </th>
+                    <th scope="col" class="px-5 py-3">
+                        Status Pembayaran
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @if (count($orders) <= 0)
+                    <tr>
+                        <td colspan="8" class="text-center py-8">Tidak ada data</td>
+                    </tr>
+                @else
+                    @foreach ($orders as $row)
+                        <tr class="bg-white border-b">
+                            <th scope="row" class="px-3 py-4 font-medium whitespace-nowrap">
+                                {{ $row->transaction_id }}
+                            </th>
+                            <th scope="row" class="px-3 py-4 font-medium whitespace-nowrap">
+                                {{ $row->user->name }}
+                            </th>
+                            <th scope="row" class="py-4 font-medium text-start whitespace-nowrap">
+                                {{ $row->user->email }}
+                            </th>
+                            <th scope="row"
+                                class="px-3 py-4 font-medium whitespace-nowrap overflow-y-auto max-w-[150px] no-scroll">
+                                {{ $row->phone }}
+                            </th>
+                            <th scope="row"
+                                class="px-3 py-4 font-medium overflow-y-auto max-w-[150px] no-scroll">
+                                {{ $row->event->event_name }}
+                            </th>
+                            <th scope="row"
+                                class="px-3 py-4 font-medium whitespace-nowrap overflow-y-auto max-w-[150px] no-scroll">
+                                Rp{{ number_format($row->amount, 0, ',', '.') }}
+                            </th>
+                            <th scope="row"
+                                class="px-3 py-4 font-medium whitespace-nowrap overflow-y-auto max-w-[150px] no-scroll">
+                                {{ $row->payment_status }}
+                            </th>
+                        </tr>
+                    @endforeach
+                @endif
+            </tbody>
+        </table>
+        <div class="mt-5 mb-5 px-5">
+            {{ $orders->links('vendor.pagination.tailwind') }}
+        </div>
+    </div>
+</div>
---
+++ resources/views/components/tables/participant.blade.php
@@ -0,0 +1,75 @@
+@props(['participants'])
+
+<div class="relative overflow-x-auto shadow-md sm:rounded-lg border-2 border-accent">
+    <x-searching :title="'Data Peserta Lomba'" :route="'participant.search'"/>
+    <div class="px-5">
+        <x-pdf-modal />
+        <table class="w-full text-sm text-center rtl:text-right text-gray-500">
+            <thead class="text-xs text-white uppercase bg-primary">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Profile
+                    </th>
+                    <th scope="col" class="px-5 py-3">
+                        Nama Tim
+                    </th>
+                    <th scope="col" class="px-5 py-3 whitespace-nowrap">
+                        Nama Peserta
+                    </th>
+                    <th scope="col" class="px-3 py-3">
+                        Email
+                    </th>
+                    <th scope="col" class="px-5 py-3 whitespace-nowrap">
+                        Universitas
+                    </th>
+                    <th scope="col" class="px-5 py-3">
+                        Role
+                    </th>
+                    <th scope="col" class="px-5 py-3">
+                        KTM
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @if (count($participants) <= 0)
+                    <tr>
+                        <td colspan="8" class="text-center py-8">Tidak ada data</td>
+                    </tr>
+                @else
+                    @foreach ($participants as $row)
+                        <tr class="bg-white border-b">
+                            <th scope="row" class="flex justify-center px-3 py-4 font-medium whitespace-nowrap">
+                                <img src="{{ asset('storage/' . $row->profile) }}" alt="product image" class="w-24 h-24 object-cover">
+                            </th>
+                            <th scope="row" class="px-3 py-4 font-medium whitespace-nowrap">
+                                {{ $row->tim->tim_name }}
+                            </th>
+                            <th scope="row" class="px-3 py-4 font-medium whitespace-nowrap">
+                                {{ $row->name }}
+                            </th>
+                            <th scope="row" class="py-4 font-medium">
+                                {{ $row->email }}
+                            </th>
+                            <th scope="row"
+                                class="px-3 py-4 font-medium overflow-y-auto max-w-[150px] no-scroll">
+                                {{ $row->university }}
+                            </th>
+                            <th scope="row"
+                                class="px-3 py-4 font-medium whitespace-nowrap overflow-y-auto max-w-[150px] no-scroll">
+                                {{ $row->role }}
+                            </th>
+                            <th scope="row" class="px-3 py-4 font-medium whitespace-nowrap">
+                                <a href="#" class="text-purple-600 hover:underline" onclick="openPDF('{{ asset('storage/' . $row->ktm) }}')">
+                                    Preview
+                                </a>
+                            </th>
+                        </tr>
+                    @endforeach
+                @endif
+            </tbody>
+        </table>
+        <div class="mt-5 mb-5 px-5">
+            {{ $participants->links('vendor.pagination.tailwind') }}
+        </div>
+    </div>
+</div>
