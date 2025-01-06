9505b9408f75a99f440beebca2cd4b56a1d445c4
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-01-06 23:41:27 +0700

    Add : Component buttons

---
+++ resources/views/components/buttons/add-event.blade.php
@@ -0,0 +1,20 @@
+<div class="grid grid-cols-1 mt-5">
+    <a href="{{ route('event.form') }}">
+        <div class="rounded-lg border-2 border-accent border-dashed bg-white px-7.5 py-6 shadow-default">
+            <div class="flex flex-col gap-2 items-center justify-center">
+                <div class="flex gap-2 justify-center items-center">
+                    <svg class="w-4 h-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                            <path d="M4 12H20M12 4V20" stroke="#433D8B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                        </g>
+                    </svg>
+                    <h4 class="text-title-xs font-bold text-secondary">
+                        Tambah
+                    </h4>
+                </div>
+            </div>
+        </div>
+    </a>
+</div>
