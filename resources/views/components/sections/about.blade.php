af788d3497f273d3f17a2213f8047d3837b6ee12
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-01-06 16:38:33 +0700

    Add : Component section guest

---
+++ resources/views/components/sections/about.blade.php
@@ -0,0 +1,6 @@
+<section id="about" class="relative flex flex-col-reverse lg:flex-row justify-center items-center overflow-hidden lg:px-12">
+    <div class="px-6 w-full max-w-screen-xl py-16 lg:py-30 text-center">
+        <h1 class="md:mb-4 text-3xl font-semibold tracking-wide text-white lg:px-12 md:text-6xl 2xl:text-7xl">Tentang</h1>
+        <p class="text-base md:text-lg mt-5 lg:mt-10 md:px-10 text-white">Tepat pada tanggal 15 November 2016, Program Studi S1 Sistem Informasi disahkan menjadi salah satu Program Studi di Institut Teknologi Telkom Purwokerto (pada saat itu) dan terus berkembang hingga saat ini memasuki usia ke – 8. Dies Natalis sebagai bentuk merayakan hari jadi Program Studi diisi dengan kegiatan hiburan yang menjadi bagian dari upaya membangun budaya organisasi baru bertujuan untuk menghilangkan sekat – sekat antargenerasi, membangun sportivitas, komitmen, dan seterusnya.</p>
+    </div>
+</section>
---
+++ resources/views/components/sections/event.blade.php
@@ -0,0 +1,18 @@
+<section id="event" class="relative flex flex-col-reverse lg:flex-row justify-center items-center overflow-hidden lg:px-12">
+    <div class="px-6 w-full max-w-screen-xl py-16 lg:py-30 text-center">
+        <h1 class="md:mb-4 text-3xl font-semibold tracking-wide text-white lg:px-12 md:text-6xl 2xl:text-7xl">Lomba</h1>
+        <div>
+            <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 mt-10">
+                <div class="flex justify-center">
+                    <img src="{{ asset('assets/img/img-event-1.png') }}" alt="event-1" class="object-contain max-w-full max-h-full">
+                </div>
+                <div class="flex justify-center">
+                    <img src="{{ asset('assets/img/img-event-2.png') }}" alt="event-2" class="object-contain max-w-full max-h-full">
+                </div>
+                <div class="flex justify-center">
+                    <img src="{{ asset('assets/img/img-event-3.png') }}" alt="event-3" class="object-contain max-w-full">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
