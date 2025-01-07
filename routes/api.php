448d44d4079f78307b53dcfa1f1938a9f4bd3e10
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-01-07 22:13:25 +0700

    Fix : API routes

--- routes/api.php
+++ routes/api.php
@@ -1,37 +1,44 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EventController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\ParticipantController;
 use App\Http\Controllers\MidtransController;
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\PasswordController;
+use App\Http\Controllers\Api\AdminController;
+use App\Http\Controllers\Api\Auth\AuthenticatedController;
+use App\Http\Controllers\Api\Auth\RegisterController;
+use App\Http\Controllers\Api\Auth\PasswordController;
+use App\Http\Controllers\Api\ParticipantController;
+use App\Http\Controllers\Api\EventController;
+use App\Http\Controllers\Api\UserController;
 
-Route::post('login', [AuthenticatedSessionController::class, 'storeJson']);
-Route::post('register', [RegisteredUserController::class, 'storeJson']);
-Route::put('password', [PasswordController::class, 'updateJson']);
+Route::post('login', [AuthenticatedController::class, 'login']);
+Route::post('register', [RegisterController::class, 'register']);
 
-Route::post('/midtrans/notifications', [MidtransController::class, 'notification']);
-
-Route::prefix('dashboard')->name('dashboard.')->group(function () {
-
-    Route::get('/orders', [DashboardController::class, 'index']);
-    Route::get('participants', [DashboardController::class, 'participant']);
-    Route::get('submissions', [DashboardController::class, 'submission']);
-
-    Route::prefix('events')->name('event.')->group(function () {
-        Route::get('/', [DashboardController::class, 'event']);
-        Route::post('form', [EventController::class, 'store']);
-        Route::get('update/{id}', [EventController::class, 'view']);
-        Route::patch('update/{id}', [EventController::class, 'update']);
-        Route::delete('delete/{id}', [EventController::class, 'destroy']);
-    });
+Route::middleware('auth:sanctum')->group(function () {
+    Route::put('password', [PasswordController::class, 'update']);
+});
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthenticatedController::class, 'destroy']);
 });
 
-Route::post('/team/register', [ParticipantController::class, 'store']);
-Route::get('/team/view/{id}', [ParticipantController::class, 'view']);
-Route::patch('/team/update/{id}', [ParticipantController::class, 'update']);
-Route::delete('/team/destroy/{id}', [ParticipantController::class, 'destroy']);
+Route::get('dashboard', [AdminController::class, 'dashboard']);
+Route::get('transactions', [AdminController::class, 'transaction']);
+Route::get('participants', [AdminController::class, 'participant']);
+Route::get('events', [AdminController::class, 'event']);
+Route::get('submissions', [AdminController::class, 'submission']);
+
+Route::post('event/store', [EventController::class, 'store']);
+Route::get('event/view/{id}', [EventController::class, 'view']);
+Route::patch('event/update/{id}', [EventController::class, 'update']);
+Route::delete('event/destroy/{id}', [EventController::class, 'destroy']);
+
+Route::post('team/store', [ParticipantController::class, 'store']);
+Route::get('team/view/{id}', [ParticipantController::class, 'view']);
+Route::patch('team/update/{id}', [ParticipantController::class, 'update']);
+Route::delete('team/destroy/{id}', [ParticipantController::class, 'destroy']);
+
+Route::get('user/view/{id}', [UserController::class, 'view']);
+Route::put('user/update/{id}', [UserController::class, 'update']);
+Route::delete('user/destroy/{id}', [UserController::class, 'destroy']);
+
+Route::post('midtrans/notifications', [MidtransController::class, 'notification']);
 
