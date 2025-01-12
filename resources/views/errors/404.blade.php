496e8839e62f942dfb344af3b22a21b32e75f4ae
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-01-12 14:03:15 +0700

    Add : Custome error handling

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('index')
+@section('content')
+    <section class="flex justify-center items-center h-[100vh]">
+        <div class="flex flex-col justify-center items-center text-white">
+            <img class="w-80" src="{{ asset('assets/error/404.png') }}" alt="">
+            <p class="text-xl font-semibold">Halaman tidak ditemukan</p>
+            <a href="/" class="mt-5 bg-primary hover:bg-secondary text-white px-4 py-2 rounded-md md:justify-self-end">Kembali ke beranda</a>
+        </div>
+    </section>
+@endsection
