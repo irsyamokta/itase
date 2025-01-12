030823f6fd4047d0f5bc61a26265cd3abd5e673a
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-01-12 12:27:15 +0700

    Add : Alert before delete team

--- resources/views/components/cards/client/team.blade.php
+++ resources/views/components/cards/client/team.blade.php
@@ -11,10 +11,10 @@
                 Update
             </a>
             @if ($tim?->order?->payment_status == null || $tim?->order?->payment_status == 'Canceled')
-                <form action="{{ route('team.destroy', Auth::user()->id) }}" method="POST">
+                <form id="delete-team-form" action="{{ route('team.destroy', Auth::user()->id) }}" method="POST">
                     @csrf
                     @method('delete')
-                    <button type="submit"
+                    <button id="delete-team-button" type="button"
                         class="bg-red-800 hover:bg-red-700 text-white text-center px-4 py-2 rounded-md">
                         Hapus
                     </button>
@@ -52,7 +52,8 @@
                     value="{{ $participants[0]->university ?? '' }}" readonly
                     class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500" />
                 <div class="flex flex-col items-start gap-2">
-                    <a href="#" class="text-purple-600 hover:underline" onclick="openPDF('{{ asset('storage/' . $participants[0]->ktm) }}')">
+                    <a href="#" class="text-purple-600 hover:underline"
+                        onclick="openPDF('{{ asset('storage/' . $participants[0]->ktm) }}')">
                         Preview
                     </a>
                 </div>
@@ -61,7 +62,8 @@
     </div>
 
     <!-- Members Loop -->
-    @foreach ($participants->skip(1) as $key => $participant) <!-- Skip leader (index 0) -->
+    @foreach ($participants->skip(1) as $key => $participant)
+        <!-- Skip leader (index 0) -->
         <div class="p-6 bg-white rounded-md shadow-md border-2 border-accent">
             <h2 class="text-xl font-semibold text-gray-700 mb-4">Member {{ $key + 1 }}</h2>
             <input type="text" class="hidden" name="role[]" value="Member" required />
@@ -81,14 +83,15 @@
                     <input type="text" placeholder="Nama Lengkap" name="name[]"
                         value="{{ $participant->name ?? '' }}" readonly
                         class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500" />
-                    <input type="email" placeholder="Email" name="email[]"
-                        value="{{ $participant->email ?? '' }}" readonly
+                    <input type="email" placeholder="Email" name="email[]" value="{{ $participant->email ?? '' }}"
+                        readonly
                         class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500" />
                     <input type="text" placeholder="Universitas" name="university[]"
                         value="{{ $participant->university ?? '' }}" readonly
                         class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500" />
                     <div class="flex flex-col items-start gap-2">
-                        <a href="#" class="text-purple-600 hover:underline" onclick="openPDF('{{ asset('storage/' . $participant->ktm) }}')">
+                        <a href="#" class="text-purple-600 hover:underline"
+                            onclick="openPDF('{{ asset('storage/' . $participant->ktm) }}')">
                             Preview
                         </a>
                     </div>
@@ -97,3 +100,24 @@
         </div>
     @endforeach
 </div>
+
+<script>
+    document.getElementById('delete-team-button').addEventListener('click', function() {
+        Swal.fire({
+            title: 'Apakah Anda yakin?',
+            text: "Data tim Anda akan dihapus!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#991b1b',
+            cancelButtonColor: '#2e236c',
+            confirmButtonText: 'Ya, hapus!',
+            cancelButtonText: 'Batal'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                document.getElementById('delete-team-form').submit();
+            }
+        });
+    });
+</script>
+
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
